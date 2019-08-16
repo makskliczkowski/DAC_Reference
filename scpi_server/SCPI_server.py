5f988a67ab6f1d014a965efe2c9000f569699047
Maks <236490@student.pwr.edu.pl>
2019-08-16 16:44:35 +0200

    Templates has been added

--- scpi_server/SCPI_server.py
+++ scpi_server/SCPI_server.py
@@ -8,15 +8,18 @@ import Adafruit_BBIO.GPIO as GPIO
 from Adafruit_BBIO.SPI import SPI
 
 
-def accept_client(sock,sel):
+def accept_client(sock, sel):
     conn, address = sock.accept()  # we accept new socket
     print("Accepted connection from", address)
     conn.setblocking(False)
     data = types.SimpleNamespace(addr=address, in_buffer=b'', out_buffer=b'')  # create info about client
-    events = selectors.EVENT_READ|selectors.EVENT_WRITE
-    sel.register(conn,e)
+    message = Message(sel, conn, address)
+    sel.register(conn, selectors.EVENT_READ, data=message)
 
-class DAC:
+
+
+
+class dac:
     def __init__(self):
         # using two's complement
         # CONSTS
@@ -42,7 +45,7 @@ class DAC:
         GPIO.setup("P9_14", GPIO.OUT)  # P4
 
         # server
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def serv_create(self, buffer=5):
         self.s.bind((self.IP, self.PORT))
@@ -50,3 +53,84 @@ class DAC:
         print(f"Creation of server on {(self.IP, self.PORT)} successful")
         self.s.setblocking(False)
 
+    # This is a class that handles the whole message before parsing it
+
+ class Message:
+    def __init__(self, selector, sock, addr):
+        self.selector = selector
+        self.sock = sock
+        self.addr = addr
+        self.received_buffer = b''
+        self.send_buffer = b''
+        self.response=None
+        self.created_response=False
+        # we can add headers and so on, depends on the needs
+
+    def set_mode(self, mode):
+        """Set selector to listen for events: mode is 'r', 'w', or 'rw'."""
+        if mode == "r":
+            events = selectors.EVENT_READ
+        elif mode == "w":
+            events = selectors.EVENT_WRITE
+        elif mode == "rw":
+            events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        else:
+            raise ValueError(f"Invalid events mask mode {repr(mode)}.")
+        self.selector.modify(self.sock, events, data=self)
+    def _read(self):
+        try:
+            # Should be ready to read
+            data = self.sock.recv(4096)
+        except BlockingIOError:
+            # Resource temporarily unavailable (errno EWOULDBLOCK)
+            pass
+        else:
+            if data:
+                self.received_buffer += data
+            else:
+                raise RuntimeError("Peer closed.")
+    def _write(self):
+        if self.send_buffer:
+            print("sending", repr(self.send_buffer), "to", self.addr)
+            try:
+                # Should be ready to write
+                sent = self.sock.send(self.send_buffer)
+            except BlockingIOError:
+                # Resource temporarily unavailable (errno EWOULDBLOCK)
+                pass
+            else:
+                self.send_buffer = self.send_buffer[sent:]
+                # Close when the buffer is drained. The response has been sent.
+                if sent and not self.send_buffer:
+                    self.close()
+
+    def process_events(self, mask):
+        if mask & selectors.EVENT_READ:
+            self._read()
+        if mask & selectors.EVENT_WRITE:
+            self._write()
+    def close(self):
+        print("closing connection to", self.addr)
+        try:
+            self.selector.unregister(self.sock)
+        except Exception as e:
+            print(
+                f"error: selector.unregister() exception for",
+                f"{self.addr}: {repr(e)}",
+            )
+
+        try:
+            self.sock.close()
+        except OSError as e:
+            print(
+                f"error: socket.close() exception for",
+                f"{self.addr}: {repr(e)}",
+            )
+        finally:
+            # Delete reference to socket object for garbage collection
+            self.sock = None
+    def create_response(self,response):
+        self.response = response
+        self.send_buffer+= response
+        self.created_response=True
+    def process_message(self):
--- scpi_server/main.py
+++ scpi_server/main.py
@@ -1,1 +1,9 @@
-from scpi_server/SCPI_server import *
+from .SCPI_server import *
+
+
+dac = dac()
+dac.__init__()
+
+sel = selectors.DefaultSelector()
+sel.register(dac.s, selectors.EVENT_READ, data=None)
+
